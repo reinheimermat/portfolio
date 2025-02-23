0654c4f1c86ec49125da53b553b7c591ca1bcb3f
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-02-23 10:54:06 -0300

    feat: Remove unused education card component; add container component for layout consistency

---
+++ src/components/ui/workflowSVG.tsx
@@ -0,0 +1,193 @@
+import * as React from 'react'
+
+export const WorkFlowSVG = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1455"
+    height="113"
+    viewBox="0 0 1455 113"
+  >
+    <title>Workflow Diagram</title>
+    <path
+      fill="none"
+      stroke="#1e1e1e"
+      strokeLinecap="round"
+      strokeWidth="2"
+      d="M32 13c32.93.9 65.25 1.03 144 0M32 13c52.38 3.61 102.54 1.71 144 0m0 0c11.91-1.58 22.04 3.93 22 22m-22-22c15.61 3.96 18.67 7.91 22 22m0 0c-3.94 13.31 1.25 26.85 0 44m0-44c.08 9.97-1.97 23.59 0 44m0 0c2.05 13.14-5.8 22.94-22 22m22-22c.51 12.44-4.22 25.37-22 22m0 0c-32.93-.64-66.07-.51-144 0m144 0c-53.81-3.2-106.46-1.9-144 0m0 0c-12.98 3.27-20.46-10.59-22-22m22 22c-14.01 4.45-22.6-10.9-22-22m0 0c-.86-13.95-2.95-24.81 0-44m0 44c-1.94-15.86-.33-32.51 0-44m0 0c3.11-18.26 6.28-19.91 22-22M10 35c2.47-13.14 3.36-20.69 22-22"
+    />
+    <text
+      y="13.436"
+      fill="#1e1e1e"
+      dominantBaseline="alphabetic"
+      fontFamily="Excalifont, Xiaolai, Segoe UI Emoji"
+      fontSize="15.251"
+      style={{ whiteSpace: 'pre' }}
+      transform="translate(38 49.937)"
+    >
+      Understand Task
+    </text>
+    <g strokeLinecap="round">
+      <path
+        fill="none"
+        stroke="#1e1e1e"
+        strokeWidth="2"
+        d="M214.99 58.27c13.14.58 63.88-.71 76.16-.15m-79.61-1.63c13.03 1.2 63.68 4.61 77.45 4.78"
+      />
+      <path
+        fill="none"
+        stroke="#1e1e1e"
+        strokeWidth="2"
+        d="M266.06 68.9c3.38-3.32 10.73-2.35 21.09-8.86m-21.61 8.99c6.79-2.87 14.47-3.9 22.72-7.86"
+      />
+      <path
+        fill="none"
+        stroke="#1e1e1e"
+        strokeWidth="2"
+        d="M266.8 51.82c3.09.16 10.28 4.6 20.35 8.22m-20.88-8.09c6.67 2.21 14.12 6.28 21.99 9.22"
+      />
+    </g>
+    <path
+      fill="none"
+      stroke="#1e1e1e"
+      strokeLinecap="round"
+      strokeWidth="2"
+      d="M335 15c58.99 1.66 112.08 4.72 144 0m-144 0c37.82-1.28 71.53-3.2 144 0m0 0c11.84-3.31 21.57 8.88 22 22m-22-22c13.89-1.37 19.38 9.96 22 22m0 0c-4.25 14.35.83 33.82 0 44m0-44c1.81 13.42.29 26.13 0 44m0 0c-1.64 15.96-10.44 19.35-22 22m22-22c4.03 13.01-5.22 24.59-22 22m0 0c-46.73-.78-91.34 2.46-144 0m144 0c-37.23-.84-74.23.97-144 0m0 0c-13.47 1.94-24.69-9.16-22-22m22 22c-11.8-3.11-25.23-2.94-22-22m0 0c3.81-16.76 1.19-30.96 0-44m0 44c.33-16.15.05-36.23 0-44m0 0c3.88-11.68 6.28-19.15 22-22m-22 22c-4.01-18.99 7.52-23.77 22-22"
+    />
+    <text
+      y="13.436"
+      fill="#1e1e1e"
+      dominantBaseline="alphabetic"
+      fontFamily="Excalifont, Xiaolai, Segoe UI Emoji"
+      fontSize="15.251"
+      style={{ whiteSpace: 'pre' }}
+      transform="translate(341 51.937)"
+    >
+      Create a solution
+    </text>
+    <g strokeLinecap="round">
+      <path
+        fill="none"
+        stroke="#1e1e1e"
+        strokeWidth="2"
+        d="M524.284 58.27c12.49.85 62.7 1.55 75.85 2.05m-71.95-3.89c13.47.16 64.74-.54 76.98-.47"
+      />
+      <path
+        fill="none"
+        stroke="#1e1e1e"
+        strokeWidth="2"
+        d="M582.534 62.83c5.87.2 12.31-.44 23.39-7.41M581.954 64c6.71-2.95 15.55-4.28 23.99-7.29"
+      />
+      <path
+        fill="none"
+        stroke="#1e1e1e"
+        strokeWidth="2"
+        d="M582.444 45.73c5.78 4.42 12.24 7.99 23.48 9.69m-24.06-8.52c6.73 2.61 15.6 6.86 24.08 9.81"
+      />
+    </g>
+    <path
+      fill="none"
+      stroke="#1e1e1e"
+      strokeLinecap="round"
+      strokeWidth="2"
+      d="M647 12c40.29-5.65 71.14.04 144 0m-144 0c28.93-3.35 60.45-.59 144 0m0 0c15.83-1.54 23.01 9.89 22 22m-22-22c10.58-2.64 21.51 4.53 22 22m0 0c-2.96 13.52-1.3 29.96 0 44m0-44c1.46 14.46-.63 27.53 0 44m0 0c3 12.46-9.14 21.12-22 22m22-22c1.32 14.85-5.58 19.82-22 22m0 0c-35.89 1.06-70.92-3.44-144 0m144 0c-40.91.23-77.07.33-144 0m0 0c-13.15-3.59-21.17-7.44-22-22m22 22c-11.1 2.8-23-3.75-22-22m0 0c2.11-9.27-.03-28.36 0-44m0 44c-.23-16.85.59-33.23 0-44m0 0c1-15.12 9.04-19.83 22-22m-22 22c-.63-14.69 4.82-21.59 22-22"
+    />
+    <g
+      fill="#1e1e1e"
+      dominantBaseline="alphabetic"
+      fontFamily="Excalifont, Xiaolai, Segoe UI Emoji"
+      fontSize="15.251"
+      textAnchor="middle"
+    >
+      <text
+        x="63.836"
+        y="13.436"
+        style={{ whiteSpace: 'pre' }}
+        transform="translate(659.3 37.937)"
+      >
+        Validate with the
+      </text>
+      <text
+        x="63.836"
+        y="32.499"
+        style={{ whiteSpace: 'pre' }}
+        transform="translate(659.3 37.937)"
+      >
+        designers team
+      </text>
+    </g>
+    <g strokeLinecap="round">
+      <path
+        fill="none"
+        stroke="#1e1e1e"
+        strokeWidth="2"
+        d="M836.863 57.494c12.87.68 64.72 5.68 78.11 6.42m-74.04-1.6c12.61-.09 59.21-2.81 71.83-2.36"
+      />
+      <path
+        fill="none"
+        stroke="#1e1e1e"
+        strokeWidth="2"
+        d="M887.623 68.904c9.19-.53 13.79-6.35 25.75-7.13m-24.9 7.46c8.93-3.02 15.79-6.69 23.91-8.67"
+      />
+      <path
+        fill="none"
+        stroke="#1e1e1e"
+        strokeWidth="2"
+        d="M887.353 51.804c9.25 3.88 13.92 2.47 26.02 9.97m-25.17-9.64c9.13 2.4 16.07 4.15 24.18 8.43"
+      />
+    </g>
+    <path
+      fill="none"
+      stroke="#1e1e1e"
+      strokeLinecap="round"
+      strokeWidth="2"
+      d="M962 10c46.39-5.57 88.21-2.61 144 0m-144 0c32.95 4.01 64.5 1.85 144 0m0 0c18.64 3.65 19.03 7.56 22 22m-22-22c18.09-2.75 19.95 3.3 22 22m0 0c-1.92 15.55 1.68 24.75 0 44m0-44c1.47 9.76 1.4 21.94 0 44m0 0c-3.92 12.7-7.38 22.45-22 22m22-22c-2.53 10.12-6.86 18.59-22 22m0 0c-37.49-4.94-79.19-2.07-144 0m144 0c-46.09.6-88.83-.51-144 0m0 0c-16.77.86-25.85-9.39-22-22m22 22c-10.35 1.83-21.69-3.68-22-22m0 0c2.67-15.92-.04-30.82 0-44m0 44c1.31-12.06-.41-21.36 0-44m0 0c-1-15.6 10.47-19.82 22-22m-22 22c-4.13-16.85 9.12-20.83 22-22"
+    />
+    <text
+      x="76.685"
+      y="13.436"
+      fill="#1e1e1e"
+      dominantBaseline="alphabetic"
+      fontFamily="Excalifont, Xiaolai, Segoe UI Emoji"
+      fontSize="15.251"
+      style={{ whiteSpace: 'pre' }}
+      textAnchor="middle"
+      transform="translate(959.452 44.937)"
+    >
+      Testings and Review
+    </text>
+    <g strokeLinecap="round">
+      <path
+        fill="none"
+        stroke="#1e1e1e"
+        strokeWidth="2"
+        d="M1151.804 53.62c13.21.19 66.76-.27 79.95.36m-75.95-2.64c13.13.59 62.35 5.28 74.66 5.56M1206.714 65.18c4.74-2.12 13.1-4.39 24.46-8.36m-24.64 6.2c9.98-2.4 18.94-5.18 24.37-5.13"
+      />
+      <path
+        fill="none"
+        stroke="#1e1e1e"
+        strokeWidth="2"
+        d="M1207.804 48.12c4.45 2.06 12.54 3.99 23.37 8.7m-23.55-10.87c9.72 4.23 18.26 8.07 23.28 11.94"
+      />
+    </g>
+    <path
+      fill="none"
+      stroke="#1e1e1e"
+      strokeLinecap="round"
+      strokeWidth="2"
+      d="M1279 12c32.27-.81 67.14 3.9 144 0m-144 0c36.33.67 74.93-2.8 144 0m0 0c14.21-.96 21.12 8.35 22 22m-22-22c17.53-1.03 26.03 5.42 22 22m0 0c-3.26 9.59 4.02 22.14 0 44m0-44c1.22 14.56-.56 30.1 0 44m0 0c1.03 13.61-5 19.67-22 22m22-22c-4.42 18.3-9.77 20.68-22 22m0 0c-45.85 3.67-90.83-.48-144 0m144 0c-43.26.13-86.67 2.68-144 0m0 0c-13.95-.69-22.63-8.46-22-22m22 22c-10.14-4.03-24.64-10.05-22-22m0 0c1.83-15.37-1.65-27.36 0-44m0 44c1.66-11.78 1.22-22.7 0-44m0 0c3.39-12.33 8.39-24.78 22-22m-22 22c.96-17.01 7.68-22.36 22-22"
+    />
+    <text
+      x="24.469"
+      y="13.436"
+      fill="#1e1e1e"
+      dominantBaseline="alphabetic"
+      fontFamily="Excalifont, Xiaolai, Segoe UI Emoji"
+      fontSize="15.251"
+      style={{ whiteSpace: 'pre' }}
+      textAnchor="middle"
+      transform="translate(1328.668 46.937)"
+    >
+      Deploy
+    </text>
+  </svg>
+)
